c25fa37b989472cbffb6401cf688599a0ecea0e7
bryanmsombra27 <bryanjm96@gmail.com>
2023-12-31 19:58:09 -0600

    pie chart added

--- src/wideOasis/features/dashboard/DashboardLayout.jsx
+++ src/wideOasis/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import Spinner from "../../ui/Spinner";
 import useRecentStays from "./useRecentStays";
 import Stats from "./Stats";
 import useCabins from "../cabins/hooks/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -13,22 +15,31 @@ const StyledDashboardLayout = styled.div`
 `;
 
 const DashboardLayout = () => {
-  const { isLoading, bookings } = useRecentBookings()
-  const { isLoading: isLoadingStays, stays, confirmStays, numDays } = useRecentStays()
-  const { isLoading: isloadingCabins, cabins } = useCabins()
-
+  const { isLoading, bookings } = useRecentBookings();
+  const {
+    isLoading: isLoadingStays,
+    stays,
+    confirmStays,
+    numDays,
+  } = useRecentStays();
+  const { isLoading: isloadingCabins, cabins } = useCabins();
 
   if (isLoading || isLoadingStays || isloadingCabins) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
-
     <StyledDashboardLayout>
-      <Stats bookings={bookings} confirmStays={confirmStays} cabinCount={cabins.length} numDays={numDays} />
+      <Stats
+        bookings={bookings}
+        confirmStays={confirmStays}
+        cabinCount={cabins.length}
+        numDays={numDays}
+      />
+      <DurationChart confirmedStays={confirmStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
-  )
-
-}
+  );
+};
 
 export default DashboardLayout;
--- src/wideOasis/features/dashboard/DurationChart.jsx
+++ src/wideOasis/features/dashboard/DurationChart.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { data } from "autoprefixer";
+import { useDarkmodeContext } from "../../context/DarkmodeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -26,22 +30,22 @@ const startDataLight = [
   },
   {
     duration: "2 nights",
-    value: 0,
+    value: 3,
     color: "#f97316",
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 5,
     color: "#eab308",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 4,
     color: "#84cc16",
   },
   {
     duration: "6-7 nights",
-    value: 0,
+    value: 7,
     color: "#22c55e",
   },
   {
@@ -51,7 +55,7 @@ const startDataLight = [
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 2,
     color: "#3b82f6",
   },
   {
@@ -130,3 +134,28 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+const DurationChart = ({ confirmedStays }) => {
+  const { isDarkMode } = useDarkmodeContext()
+  const startData = isDarkMode ? startDataDark : startDataLight
+  const data = prepareData(startData, confirmedStays)
+  return (
+
+    <ChartBox>
+      <Heading as="h2">Stay duration summary </Heading>
+      <ResponsiveContainer width="100%" height={240}>
+        <PieChart>
+          <Pie data={data} nameKey="duration" dataKey="value" innerRadius={85} outerRadius={110} cx="40%" cy="50%" paddingAngle={3}>
+            {data.map(entry => <Cell fill={entry.color} stroke={entry.color} key={entry.duration} />)}
+          </Pie>
+          <Tooltip />
+          <Legend verticalAlign="middle" align="right" width="30%" layout="vertical" iconSize={15} iconType="circle" />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+
+  )
+
+}
+
+export default DurationChart;
